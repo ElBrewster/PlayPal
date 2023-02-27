2e0a11df5c7cf6b8c75c914d855a96938152c8b4
Patt Sookmark <pijiya@sookmark.com>
2023-02-27 11:09:13 -0700

    Adding Cy-testing profile

--- ui/cypress/e2e/profile_page.cy.js
+++ ui/cypress/e2e/profile_page.cy.js
@@ -1,5 +1,16 @@
 describe("profile page", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000/Profile");
+  });
   describe("When profile page render correctly", () => {
-    it("Should render activity card correctly ", () => {});
+    it("Should render activity card correctly ", () => {
+      cy.get(".header-nav");
+      //header nav exist // we test this in other file already
+      cy.get(".report-h2").contains(" Kiddo Report Card");
+      //header say 'Kiddo Report Card'
+      //6 paragraph tags
+      cy.get(".report").find(".report-p").should("have.length", 6);
+    });
+    // Once we have import activities booked mark, sad path will be having response message where there's no activities
   });
 });
