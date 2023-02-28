3be38e060b435ceceaddd06c0c0ee5d506cc6d53
ElBrewster <113723085+ElBrewster@users.noreply.github.com>
2023-02-27 19:11:12 -0800

    Add updated remove and delete icons to all activities

--- ui/src/components/thumbnailActivity/thumbnailActivityCard.js
+++ ui/src/components/thumbnailActivity/thumbnailActivityCard.js
@@ -1,13 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import "./thumbnailActivityCard.scss";
-import { AiFillCheckCircle, AiOutlineHeart, AiFillHeart, AiOutlineCheckCircle } from "react-icons/ai";
-import { BsBookmark, BsFillBookmarkFill } from "react-icons/bs";
+import { RiHeartAddFill } from "react-icons/ri";
+import { TiDelete } from "react-icons/ti";
 
 
 export default function ActivityCard({id, name, startAge, endAge, image, activities, savedActivities, setSavedActivities}) {
-//   const [toggle, setToggle] = useState(false);
-//   const [toggleBookmark, setToggleBookmark] = useState();
 
   function addToSavedActivities(grabbedId) {
 	const checked = savedActivities.find(activity => activity.id === grabbedId);
@@ -22,24 +20,14 @@ export default function ActivityCard({id, name, startAge, endAge, image, activit
   function removeFromSavedActivities(grabbedId){
 	const unbookMarked = savedActivities.filter(activity => activity.id !== grabbedId);
 	setSavedActivities(unbookMarked);
-	console.log(activities);
   }
 
-//   const handleToggle = () => {
-//     setToggle(!toggle);
-//   };
-
   const handleBookMarkClickEmpty = () => {
-	// setToggleBookmark(true);
 	addToSavedActivities(id)
-
-	console.log("smile");
   }
 
   const handleBookMarkClickFull = () => {
-	// setToggleBookmark(false);
 	removeFromSavedActivities(id);
-	console.log("FROWN");
   }
 
 	return (
@@ -48,29 +36,17 @@ export default function ActivityCard({id, name, startAge, endAge, image, activit
 					<p className='activity-name'> {name} </p>
 					<p className="activity-age"> {startAge} months-{endAge} months </p>
 				</div>
-
 				<div className="pics-container">
-
 					<Link to={`/Activities/${id}`} state={{id: id, activities: activities}} style={{ textDecoration: 'none' }} className="activity-link">
 						<img className="activity-image" src={image} alt={name} width={300} />
 					</Link>
-
 					<div className="tv-dials">
-						{/* {!toggle && <AiOutlineHeart className="heartmark" onClick={handleToggle} type='button' alt='heart icon'/>}
-						{toggle && <AiFillHeart className="heartmark2" onClick={handleToggle} type='button' alt='filled heart icon'/>} */}
                     <button className="invis-button" onClick={handleBookMarkClickEmpty}>
-                        <BsBookmark className="bookmark" alt="bookmark icon" />
-						<p>ADD</p>
+                        <RiHeartAddFill className="heartmark2" alt="heart plus icon" />
                     </button>
 					<button className="invis-button" onClick={handleBookMarkClickFull}>
-                        <BsFillBookmarkFill className="bookmark2" alt="bookmark icon" />
-						<p>REMOVE</p>
+                        <TiDelete className="bookmark2" alt="remove icon" />
                     </button>
-						{/* {!toggleBookmark && <BsBookmark className="bookmark" onClick={handleBookMarkClickEmpty} type='button' alt='empty bookmark icon'/>}
-						{toggleBookmark && <BsFillBookmarkFill className="bookmark2" onClick={handleBookMarkClickFull} type='button' alt='filled bookmark icon'/>} */}
-
-						{/* {!toggle && <AiOutlineCheckCircle className="check-circle" onClick={handleToggle} type='button' alt='empty checkmark circle icon'/>}
-						{toggle && <AiFillCheckCircle className="check-circle2" onClick={handleToggle} type='button' alt='filled checkmark circle icon'/>} */}
 					</div>
 				</div>
 		</section>
