abc2250890004c54609ff6563ec196049e17026e
ElBrewster <113723085+ElBrewster@users.noreply.github.com>
2023-02-27 17:19:26 -0800

    Add new icons to landing page

--- ui/src/components/headerNav/HeaderNav.js
+++ ui/src/components/headerNav/HeaderNav.js
@@ -1,8 +1,10 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import "./HeaderNav.scss";
-import { BiGame } from "react-icons/bi";
-import { IoIosClipboard } from "react-icons/io"
+import { BiGame, BiHeartSquare } from "react-icons/bi";
+import { BsBookmarkHeartFill, BsHeartFill } from "react-icons/bs";
+// import { IoIosClipboard } from "react-icons/io";
+
 
 export default function HeaderNav() {
     const navigate = useNavigate();
@@ -10,13 +12,13 @@ export default function HeaderNav() {
         <header className="header-container">
                 <nav className="header-nav">
                     <button className="invis-button" onClick={() => navigate("/Saved-Activities")}>
-                        <IoIosClipboard className="clipboard" alt="bookmark icon" />
+                        <BiGame className="clipboard" alt="bookmark icon" />
                     </button>
                     <button className="invis-button" onClick={() => navigate("/")}>
                         <h1 className="main-title">PlayPals</h1>   
                     </button >
                     <button className="invis-button" onClick={() => navigate("/Profile")}>
-                        <BiGame className="dude" alt="little dude icon" />      
+                        <BiHeartSquare className="dude" alt="little dude icon" />      
                     </button>  
                 </nav>
         </header>
--- ui/src/components/landingPage/LandingPage.js
+++ ui/src/components/landingPage/LandingPage.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import "./LandingPage.scss";
+import { RiHeartAddFill, RiGameLine } from "react-icons/ri";
 
 
 export default function LandingPage() {
@@ -13,8 +14,8 @@ export default function LandingPage() {
                 <p className="hello-p">Let's play with Maddie!</p>
             </section>
             <div className="button-box">
-                <button className="find" onClick={() => navigate("/Activities")} >Find a New Activity</button>
-                <button className="go" onClick={() => navigate("/Saved-Activities")} >Go to Saved Activities</button>
+                <button className="find" onClick={() => navigate("/Activities")} >Find a New Activity <RiHeartAddFill className="filled-heart"/></button>
+                <button className="go" onClick={() => navigate("/Saved-Activities")} >Go to Saved Activities <RiGameLine className="biter"/></button>
             </div>
         </section>
     );
