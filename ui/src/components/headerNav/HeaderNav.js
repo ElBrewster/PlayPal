200306eaec6a2980b6963c9d9f569100f1f00616
ElBrewster <113723085+ElBrewster@users.noreply.github.com>
2023-02-27 18:39:34 -0800

    Add updated icons to header

--- ui/src/components/headerNav/HeaderNav.js
+++ ui/src/components/headerNav/HeaderNav.js
@@ -1,43 +1,8 @@
-// import React from "react";
-// import { useNavigate } from "react-router-dom";
-// import "./HeaderNav.scss";
-// import { BiGame, BiHeartSquare } from "react-icons/bi";
-// import { BsBookmarkHeartFill, BsHeartFill } from "react-icons/bs";
-// import { BsSuitHeartFill } from "react-icons/bs"
-// import { IoIosClipboard } from "react-icons/io"
-
-
-// export default function HeaderNav() {
-//     const navigate = useNavigate();
-//     return (
-//         <header className="header-container">
-//                 <nav className="header-nav">
-//                     <button className="invis-button" onClick={() => navigate("/Saved-Activities")}>
-//                         <BiGame className="clipboard" alt="bookmark icon" />
-
-//                     </button>
-//                     <button className="invis-button" onClick={() => navigate("/")}>
-//                         <h1 className="main-title">PlayPals</h1>   
-//                     </button >
-
-//                     {/* <button className="invis-button" onClick={() => navigate("/Profile")}>
-//                         <BiHeartSquare className="dude" alt="little dude icon" />       */}
-
-//                     <button className="invis-button" onClick={() => navigate("/favorite-Activities")}>
-//                         <BsSuitHeartFill className="dude" alt="little dude icon" />      
-
-//                     </button>  
-//                 </nav>
-//         </header>
-//     );
-// }
-
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import "./HeaderNav.scss";
-import { BsSuitHeartFill } from "react-icons/bs"
-// import { BiGame } from "react-icons/bi";
-import { IoIosClipboard } from "react-icons/io"
+import { BiGame, BiHeartSquare } from "react-icons/bi";
+
 
 export default function HeaderNav() {
     const navigate = useNavigate();
@@ -45,13 +10,13 @@ export default function HeaderNav() {
         <header className="header-container">
                 <nav className="header-nav">
                     <button className="invis-button" onClick={() => navigate("/dashboard")}>
-                        <IoIosClipboard className="clipboard" alt="bookmark icon" />
+                        <BiGame className="clipboard" alt="bookmark icon" />
                     </button>
                     <button className="invis-button" onClick={() => navigate("/")}>
                         <h1 className="main-title">PlayPals</h1>   
                     </button >
                     <button className="invis-button" onClick={() => navigate("/favorite-Activities")}>
-                        <BsSuitHeartFill className="dude" alt="little dude icon" />      
+                        <BiHeartSquare className="dude" alt="little dude icon" />      
                     </button>  
                 </nav>
         </header>
--- ui/src/components/landingPage/LandingPage.js
+++ ui/src/components/landingPage/LandingPage.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import "./LandingPage.scss";
-// import { RiHeartAddFill, RiGameLine } from "react-icons/ri";
+import { RiHeartAddFill, RiGameLine } from "react-icons/ri";
 import Form from "../form/Form";
 
 
@@ -21,11 +21,8 @@ export default function LandingPage({careGiverName, setCareGiverName, childName,
                 <p className="hello-p">Let's play with {childName}!</p>
             </section>
             <div className="button-box">
-
-                {/* // <button className="find" onClick={() => navigate("/Activities")} >Find a New Activity <RiHeartAddFill className="filled-heart"/></button>
-                // <button className="go" onClick={() => navigate("/Saved-Activities")} >Go to Saved Activities <RiGameLine className="biter"/></button> */}
-                <button className="find" onClick={() => navigate("/dashboard")} >Find a New Activity</button>
-                <button className="go" onClick={() => navigate("/favorite-Activities")} >Go to Favorite Activities</button>
+                <button className="find" onClick={() => navigate("/dashboard")} >Find a New Activity <RiHeartAddFill className="filled-heart"/></button>
+                <button className="go" onClick={() => navigate("/favorite-Activities")} >Go to Favorite Activities <RiGameLine className="biter"/></button>
             </div>
         </section>
     );
